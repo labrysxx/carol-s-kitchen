d0588998398df022acbc6aa0b75da840da2abc44
Caroline Faria <caroline.gdfaria@gmail.com>
2023-06-27 16:26:10 -0300

    :sparkles: feat: comunicando com o banco de dados e criando card da receita

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -16,71 +16,17 @@ let currentPage
 let currentButton
 
 
-fetch("js/db.json")
+fetch("http://localhost:3333/receitas")
     .then((response) => response.json())
     .then(function(data) {
+        console.log(data)
         //verificação da localização da pagina
         if(url.match(salgadosPage)) {
-            dataRecipe = data.salgados
+            dataRecipe = data.filter((item) => item.categoria === 'salgados')
         } else if(url.match(docesPage)) {
-            dataRecipe = data.doces
+            dataRecipe = data.filter((item) => item.categoria === 'doces')
         }
-        return dataRecipe.map(function(receita) {
-            //criando elementos
-            let divAll = createNode('div')
-            divAll.classList.add('card')
-            let divImage = createNode('div')
-            let img = createNode('img')
-            img.setAttribute('alt', `imagem da receita de ${receita.nome}`)
-            let divText = createNode('div')
-            let title = createNode('p')
-            title.classList.add('title-card')
-            let description = createNode('p')
-            let author = createNode('p')
-            author.classList.add('small-text')
-            let porcao = createNode('p')
-            porcao.classList.add('small-text')
-            let botao = createNode('button')
-            botao.setAttribute('id', `${receita.id}`)
-
-            //atribuindo valores a eles
-            img.src = receita.imagem
-            title.innerHTML = `${receita.nome}`
-            description.innerHTML = `${receita.descricao}`
-            author.innerHTML = `por: <a href=${receita.contato} target="_blank">${receita.autor}</a>`
-            porcao.innerHTML = `serve: ${receita.rendimento}`
-            botao.innerHTML = 'Ver Receita'
-
-            //chamando eles
-            append(divImage, img)
-            append(divText, title)
-            append(divText, description)
-            append(divText, author)
-            append(divText, porcao)
-            append(divText, botao)
-            append(divAll, divImage)
-            append(divAll, divText)
-            append(section, divAll)
-
-            botao.addEventListener('click', (e) => {
-
-                currentPage = window.location.href.split('assets/')[1]
-                idBotao = e.target.id
-
-                fetch("js/db.json")
-                    .then((response) => response.json())
-                    .then(function(data) {
-                        if(currentPage === 'salgados.html') {
-                            currentButton = data.salgados
-                        } else if(currentPage === 'doces.html') {
-                            currentButton = data.doces
-                        }
-                        console.log(currentButton[idBotao])
-                    })
-
-            })
-
-        })
+        createCardRecipe()
 
     })
     .catch(function(error) {
@@ -88,10 +34,59 @@ fetch("js/db.json")
     })
 
 
+function openRecipePage() {
+    const buttons = document.querySelectorAll('button')
+    const title = document.querySelector('.title')
+    for(let button of buttons) {
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+            section.innerHTML = ''
+            title.innerHTML = `Receita de ${e.target.parentNode.parentNode.querySelector('.title-card').innerHTML}`
 
+        })
+    }
+}
 
+function createCardRecipe() {
+    dataRecipe.forEach((receita) => {
+        //criando elementos
+        let divAll = createNode('div')
+        divAll.classList.add('card')
+        let divImage = createNode('div')
+        let img = createNode('img')
+        img.setAttribute('alt', `imagem da receita de ${receita.nome}`)
+        let divText = createNode('div')
+        let title = createNode('p')
+        title.classList.add('title-card')
+        let description = createNode('p')
+        let author = createNode('p')
+        author.classList.add('small-text')
+        let porcao = createNode('p')
+        porcao.classList.add('small-text')
+        let botao = createNode('button')
+        botao.setAttribute('id', `${receita.id}`)
 
+        //atribuindo valores a eles
+        img.src = `${receita.imagem}`
+        title.innerHTML = `${receita.nome}`
+        description.innerHTML = `${receita.descricao}`
+        author.innerHTML = `por: <a href=${receita.contato} target="_blank">${receita.autor}</a>`
+        porcao.innerHTML = `serve: ${receita.rendimento}`
+        botao.innerHTML = 'Ver Receita'
 
+        //chamando eles
+        append(divImage, img)
+        append(divText, title)
+        append(divText, description)
+        append(divText, author)
+        append(divText, porcao)
+        append(divText, botao)
+        append(divAll, divImage)
+        append(divAll, divText)
+        append(section, divAll)
+    })
+    openRecipePage()
+}
 
 
 
