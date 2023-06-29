0ba520d4d018bfdef016ba338590ae4ea060a69b
Caroline Faria <caroline.gdfaria@gmail.com>
2023-06-29 13:44:55 -0300

    :sparkles: feat: adicionando funcionalidade de filtrar receitas: configurando localStorage

---
+++ assets/js/filter.js
@@ -0,0 +1,75 @@
+const filter_input = document.querySelector('.input_ingredient')
+let recipes = Array()
+
+filter_input.addEventListener('keydown', (e) => {
+    if(e.keyCode === 13) {
+        e.preventDefault()
+        addFilter(e)
+    }
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    localStorage.clear()
+})
+
+function updateLocalStorage() {
+    localStorage.setItem('recipes', JSON.stringify(recipes))
+}
+
+function checkLocalStorage() {
+    recipes = JSON.parse(localStorage.getItem('recipes'))
+}
+
+function addFilter(e) {
+    let chosenIngredient = e.target.value.toLowerCase()
+    const filters = document.querySelector('.filters')
+
+    let divIngredient = document.createElement('div')
+    const deleteIngredient = document.createElement('div')
+
+    deleteIngredient.innerHTML = 'x'
+    divIngredient.innerHTML = chosenIngredient.toLowerCase()
+    divIngredient.classList.add('ingredientFiltered');
+    deleteIngredient.classList.add('deleteBtn')
+    divIngredient.dataset.ingredient = chosenIngredient
+
+    divIngredient.appendChild(deleteIngredient)
+    filters.insertAdjacentElement('beforeend', divIngredient);
+
+    recipes.push(chosenIngredient);
+    updateLocalStorage();
+
+    clearForm()
+    showFilteredRecipes()
+}
+
+function clearForm() {
+    filter_input.value = ''
+}
+
+function showFilteredRecipes() {
+    const filteredRecipes = dataRecipe.filter((recipe) => {
+        return recipes.every((chosenIngredient) => {
+            return recipe.ingredientes.some((ingredient) => {
+                return ingredient.includes(chosenIngredient);
+            });
+        });
+    });
+    deleteIngredient()
+    //console.log(filteredRecipes);
+}
+
+function deleteIngredient() {
+    const deleteFilterBtns = document.querySelectorAll('.deleteBtn')
+    for (const deleteBtn of deleteFilterBtns) {
+        deleteBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            checkLocalStorage()
+            recipes = recipes.filter(ingredient => ingredient !== e.target.parentNode.dataset.ingredient)
+            e.target.parentNode.remove()
+            updateLocalStorage();
+            showFilteredRecipes()
+        })
+    }
+}
+
